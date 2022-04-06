ec439c9689c75e2959e9b07aa7abb4968d6be0f6
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2022-04-06 12:24:24 +0530

    bug fix

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,25 +1,25 @@
-import React from 'react'
-// import { useParams } from 'react-router-dom'
-// import { useFetch } from '../useFetch'
+// import React from 'react'
+// // import { useParams } from 'react-router-dom'
+// // import { useFetch } from '../useFetch'
 
-function SingleMovie({ clickedId }) {
-  //   const { id } = useParams()
-  const SINGLE = `https://api.themoviedb.org/3/movie/${clickedId}?api_key=${process.env.REACT_APP_API_KEY}`
+// function SingleMovie({ clickedId }) {
+//   //   const { id } = useParams()
+//   const SINGLE = `https://api.themoviedb.org/3/movie/${clickedId}?api_key=${process.env.REACT_APP_API_KEY}`
 
-  const { movie, loading } = useFetch(SINGLE)
-  const { title, overview, release_date, genres, runtime } = movie
-  //   console.log(movie, loading)
-  return (
-    <div className='movie-details'>
-      <h1>{title}</h1>
-      <p>{overview}</p>
-      <div className='details'>
-        <span>{release_date}</span>
-        {/* <span>{genres}</span> */}
-        <span>{runtime}</span>
-      </div>
-    </div>
-  )
-}
+//   const { movie, loading } = useFetch(SINGLE)
+//   const { title, overview, release_date, genres, runtime } = movie
+//   //   console.log(movie, loading)
+//   return (
+//     <div className='movie-details'>
+//       <h1>{title}</h1>
+//       <p>{overview}</p>
+//       <div className='details'>
+//         <span>{release_date}</span>
+//         {/* <span>{genres}</span> */}
+//         <span>{runtime}</span>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default SingleMovie
+// export default SingleMovie
