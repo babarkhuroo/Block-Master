976b96614ff147e8b326f64f4c83b0de076c585e
babar <babbark32@gmail.com>
2022-09-16 15:01:56 +0530

    tweaked footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import styles from './Footer.module.css'
 function Footer() {
   return (
     <footer className={styles.footer}>
-      &copy; {new Date().getFullYear()} <span>Sheikh Camel</span>
+      <p>
+        &copy; {new Date().getFullYear()}
+        <span>Sheikh Camel</span>
+      </p>
       <p>All rights reserved.</p>
     </footer>
   )
